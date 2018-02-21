96ffb5ceaa83cb3b2137a86274decb2b343caf59
Sebastian <sanctuary@yandex.ru>
2018-02-21 13:49:55 +0500

    финальный билд

--- bus/Source.cpp
+++ bus/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <ctime>
 #include <Windows.h>
@@ -18,26 +19,30 @@ class driver
 public:
 	driver()
 	{
-		EnterDrop();
-		cout << "\nВведите имя водителя: ";
-		getline(cin, s_name);
-		cout << "\nВведите марку автобуса: ";
-		getline(cin, s_brand);
-		cout << "\nВведите номер автобуса: ";
-		cin >> i_number;
-		cout << "\nВведите маршрут автобуса: ";
-		cin >> i_route;
-		cout << "\nВведите год выпуска автобуса: ";
-		cin >> i_year;
-		cout << "\nВведите пробег автобуса: ";
-		cin >> i_race;
+		s_name = "none";
+		s_brand = "none";
+		i_number = 0;
+		i_route = 0;
+		i_year = 0;
+		i_race = 0;
 	}
 
+	void file_read(fstream &ifs);
 	void print();
 	void print_route(int i_key);
 	void print_age(int i_key);
 	void print_race(int i_key);
 };
+
+void driver::file_read(fstream &ifs)
+{
+	getline(ifs, s_name);
+	getline(ifs, s_brand);
+	ifs >> i_number;
+	ifs >> i_route;
+	ifs >> i_year;
+	ifs >> i_race;
+}
 
 void driver::print()
 {
@@ -47,6 +52,7 @@ void driver::print()
 	cout << "\nМаршрут: " << i_route;
 	cout << "\nГод: " << i_year;
 	cout << "\nПробег: " << i_race;
+	cout << "\n==============================" << endl;
 }
 
 void driver::print_route(int i_key)
@@ -61,7 +67,7 @@ void driver::print_age(int i_key)
 
 void driver::print_race(int i_key)
 {
-	if (i_race > i_key) print();
+	if (i_race > i_key)	print();
 }
 
 void main()
@@ -70,47 +76,45 @@ void main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	cout << "СПИСОК МАРШРУТОВ\n";
+	fstream ifs;
 	int i, i_quant, i_key;
-	cout << "\nВведите число водителей: ";
-	cin >> i_quant;
+	ifs.open("file.txt", ios::in);
+	ifs >> i_quant;
 	driver *list = new driver[i_quant];
+	for (i = 0; i < i_quant; i++) list[i].file_read(ifs);
+
 	for (i = 0; i < i_quant; i++)
 	{
-		cout << "\n==============================" << endl;
 		list[i].print();
-
 	}
 
-	cout << "\nНомер маршрута? ";
+	cout << "\n==============================\nВведите номер маршрута для вывода: ";
 	cin >> i_key;
-	cout << "\nАвтобусы с маршрутом " << i_key << " :";
+	cout << "Автобусы с маршрутом " << i_key << ": ";
 	for (i = 0; i < i_quant; i++)
 	{
-		cout << "\n==============================" << endl;
 		list[i].print_route(i_key);
-
 	}
 
-	cout << "\nГод? ";
+	cout << "\n==============================\nВведите год для проверки пробега: ";
 	cin >> i_key;
-	cout << "\nАвтобусы с пробегом больше 10 лет к " << i_key << " году:";
+	cout << "Автобусы с пробегом больше 10 лет к " << i_key << " году:";
+
 	for (i = 0; i < i_quant; i++)
 	{
-		cout << "\n==============================" << endl;
 		list[i].print_age(i_key);
-
 	}
 
-	cout << "\nПробег? ";
+	cout << "\n==============================\nВведите предел пробега (в км.): ";
 	cin >> i_key;
-	cout << "\nАвтобусы с пробегом больше " << i_key << " км.:";
+	cout << "Автобусы с пробегом больше " << i_key << " км.:";
 	for (i = 0; i < i_quant; i++)
 	{
-		cout << "\n==============================" << endl;
 		list[i].print_race(i_key);
-
 	}
 
+	cout << endl << endl;
 	delete[] list;
 	system("@pause");
 }
